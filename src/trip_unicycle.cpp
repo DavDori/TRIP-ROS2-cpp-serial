eb486c57a73cbd76390a731b4451fc7a8924abab
DavDori <davidedorigoni@gmail.com>
2024-02-03 20:36:40 +0100

    add models and its parameters

--- src/trip_unicycle.cpp
+++ src/trip_unicycle.cpp
@@ -37,20 +37,28 @@ private:
 public:
     RobotNode() : rclcpp::Node("trip")
     {
-        double ppr, max_rpm;
+        double ppr;
+        double max_rpm;
+        double wheel_radius, wheel_distance, gearbox;
         std::string port;
         long baud_rate;
         int rate_feedback;
 
         this->declare_parameter("port", "/dev/ttyACM0");
         this->declare_parameter("baudrate", 9600);
-        this->declare_parameter("motor_max_rpm", 60.0);
-        this->declare_parameter("pub_rate_feedback_Hz", 20);
+        this->declare_parameter("motor_max_rpm", 20.0);
+        this->declare_parameter("sprocket_radius_m", 0.035);
+        this->declare_parameter("track_distance_m", 0.16);
+        this->declare_parameter("gearbox_ratio", 1.0);
+        this->declare_parameter("pub_rate_feedback_Hz", 1);
         this->declare_parameter("pulse_per_revolution", 1024.0);
 
         this->get_parameter("port", port);
         this->get_parameter("baudrate", baud_rate);
         this->get_parameter("motor_max_rpm", max_rpm);
+        this->get_parameter("sprocket_radius_m", wheel_radius);
+        this->get_parameter("track_distance_m", wheel_distance);
+        this->get_parameter("gearbox_ratio", gearbox);
         this->get_parameter("pub_rate_feedback_Hz", rate_feedback);
         this->get_parameter("pulse_per_revolution", ppr);
 
@@ -63,6 +71,7 @@ public:
                 std::vector<std::shared_ptr<Encoder>>{EncoderLeft,EncoderRight}));
             MotorLeft.reset(new Motor(1, Device));
             MotorRight.reset(new Motor(2, Device));
+            Model.reset(new DifferentialDriveModel(wheel_radius, wheel_distance, 1.0));
         }
         catch(errors code){
             printErrorCode(code);
